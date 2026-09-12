20158d20a6dbebfb00f4a5dff1fb8decb84c3211
Jiří Beneš <mail@jiribenes.com>
2026-09-12 18:53:13 +0200

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,11 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     effekt-nix = {
       url = "github:jiribenes/effekt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows ="flake-utils";
     };
   };
 
